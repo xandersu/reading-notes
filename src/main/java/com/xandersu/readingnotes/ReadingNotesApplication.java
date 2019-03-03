7a75639453fa4c3b33655c60498cd9ae30c81225
xandersu <840595532@qq.com>
2019-03-03 10:54:48 +0800

    Initial commit

---
+++ src/main/java/com/xandersu/readingnotes/ReadingNotesApplication.java
@@ -0,0 +1,13 @@
+package com.xandersu.readingnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xandersu/readingnotes/ReadingNotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xandersu.readingnotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadingNotesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
