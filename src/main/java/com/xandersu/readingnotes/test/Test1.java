07a4f61f646acf6589e2c80eaa2e22e9f95c29be
xandersu <840595532@qq.com>
2019-03-24 15:56:34 +0800

    update 第六章

---
+++ src/main/java/com/xandersu/readingnotes/test/Test1.java
@@ -0,0 +1,17 @@
+package com.xandersu.readingnotes.test;
+
+/**
+ * @Author: suxun
+ * @Date: 2019/3/24 15:50
+ * @Description:
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        Double a = 50D;
+        Double b = 0D;
+        Double v = a / b - a / b;
+        System.out.println(v);
+
+    }
+}
